7ad99f837efc0f3994ac950b98b51ad8a034fb26
karasu <karasu@antergos.com>
2017-09-04 09:54:21 +0200

    Fix dbus config files

--- welcomed/dbus/install-dbus-polkit.sh
+++ welcomed/dbus/install-dbus-polkit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-install -m644 com.antergos.welcomed.service /usr/share/dbus-1/system-services
-install -m644 com.antergos.welcomed.conf /etc/dbus-1/system.d
-install -m644 com.antergos.welcomed.policy /usr/share/polkit-1/actions
-#sudo sed -i "s|USER|${USER}|g" /usr/share/dbus-1/system-services/com.antergos.welcomed.service
+install -m644 com.antergos.welcome.service /usr/share/dbus-1/system-services
+install -m644 com.antergos.welcome.conf /etc/dbus-1/system.d
+install -m644 com.antergos.welcome.policy /usr/share/polkit-1/actions
+#sudo sed -i "s|USER|${USER}|g" /usr/share/dbus-1/system-services/com.antergos.welcome.service
