d9043b3bbfedf4359f9798db7b85451e40050627
karasu <gustaucastells@gmail.com>
2017-09-12 07:03:39 +0200

    Added service file. Moved dbus folder

--- dbus/install-dbus-polkit.sh
+++ dbus/install-dbus-polkit.sh
@@ -2,4 +2,5 @@
 install -m644 com.antergos.welcome.service /usr/share/dbus-1/system-services
 install -m644 com.antergos.welcome.conf /etc/dbus-1/system.d
 install -m644 com.antergos.welcome.policy /usr/share/polkit-1/actions
+install -m644 welcomed.service /usr/lib/systemd/system
 #sudo sed -i "s|USER|${USER}|g" /usr/share/dbus-1/system-services/com.antergos.welcome.service
